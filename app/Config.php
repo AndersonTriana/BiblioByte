3b7c9acbc009a5f0a48e83369d75b7f823c270d5
Anderson Triana <andersontrianag@gmail.com>
2024-01-29 10:06:29 -0500

    Added config file

---
+++ app/Config.php
@@ -0,0 +1,7 @@
+<?php
+// config.php
+
+define('STORAGE_PATH', 'storage/'); 
+define('MAX_BOOK_NAME_LENGTH', 100);
+define('MAX_BOOK_FILE_SIZE', 30000000); //30MB
+define('MAX_COVER_FILE_SIZE', 2000000); //2MB
